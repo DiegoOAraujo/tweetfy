df801d6b3a4e49a0234f5b6287d8729894e30a8d
DiegoOAraujo <doasalah@gmail.com>
2018-08-23 00:48:23 -0300

    model de usuario e autoload

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const app = require('express')();
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 const dbConfig = require('./config/database');
 
 mongoose.connect(dbConfig.url);
+requireDir(dbConfig.modelsPath);
+
+const User = mongoose.model('User');
+User.create({
+  name: 'Diego',
+  username: 'diegoOA',
+  email: 'diego.araujo.lds@gmail.com',
+  password: '123456',
+},() => {
+  console.log('OK');
+});
 
 app.listen(3000);
